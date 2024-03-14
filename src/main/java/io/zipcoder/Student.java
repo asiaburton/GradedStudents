bfc2791d349814ef7b0995970551bcff9a929684
Asia <asia@Zipcoders-MacBook-Pro-18.local>
2024-03-14 19:11:46 -0400

    intial commit, group progress

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,59 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+    // instance variables
+
+    private String firstname;
+    private String lastname;
+
+    private ArrayList<Double> examScores;
+
+
+
+    public Student(String firstname, String lastname, ArrayList<Double> examScores) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.examScores = examScores;
+    }
+
+
+    public String getFirstname() {
+        return this.firstname;
+    }
+
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public double getExamScore() {
+
+    }
+
+    public double getNumberOfTestsTaken() {
+        // count indexes of exams taken for student
+        //ArrayList<Double>
+    }
+
+    public String getExamScores() {
+        String returnExamScores = "";
+        for( int i = 0; i <examScores.size(); i++) {
+            returnExamScores += "\n" + examScores;
+        }
+        return returnExamScores;
+    }
+
+    public void addExamScore(double examScore) {
+    }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,61 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentTest {
+
+@Test
+    public void testGetFirstname() {
+
+    }
+@Test
+    public void testSetFirstname() {
+    }
+@Test
+    public void testGetLastname() {
+    }
+@Test
+    public void testSetLastname() {
+    }
+
+ @Test
+ public void testAddExamScore() {
+    // : Given
+     String firstName = "Leon";
+     String lastName = "Hunter";
+     ArrayList<Double> examScores = new ArrayList<>();
+     Student student = new Student(firstName, lastName, examScores);
+
+    // When
+     String expectedExamScore = "Exam Scores: \n" + 100;
+     student.addExamScore(100);
+     String actualExamScore = student.getExamScores();
+
+    // Then
+     Assert.assertEquals(expectedExamScore, actualExamScore);
+
+ }
+
+ @Test
+    public void testSetExamScore() {
+     // : Given
+     String firstName = "Leon";
+     String lastName = "Hunter";
+     ArrayList<Double> examScores = new ArrayList<>(List.of(100.0, 95.0, 123.0, 96.0));
+     Student student = new Student(firstName, lastName, examScores);
+
+     // When
+     student.addExamScore(100.0);
+     String output = student.getExamScores();
+
+     // Then
+     Assert.assertEquals(expectedExamsScores, actualExamScores);
+
+
+ }
 
 }
